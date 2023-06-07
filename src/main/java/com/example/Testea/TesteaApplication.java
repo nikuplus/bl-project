7d34b948cbbab4ce4e66ab1e7e9a0a0a8c4c2900
nikuplus <raito_kanata@yahoo.co.jp>
2023-06-07 20:26:11 +0900

    サードコミット

---
+++ src/main/java/com/example/Testea/TesteaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Testea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Testea/TesteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Testea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
